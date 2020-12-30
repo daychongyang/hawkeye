fff2b51efd06ffbae8c730c269cfc619670ce9f2
daychongyang <daychongyang@gmail.com>
2020-12-30 14:51:16 +0800

    feat: logger

--- src/common/index.ts
+++ src/common/index.ts
@@ -10,19 +10,16 @@ interface Options {
   listeningListener?: () => void
 }
 
-const listener = (host: string, port: number) => {
-  console.log(`Server is running at: http://${host}:${port}`)
-}
-
 export function createServer(options: Options) {
-  const { host = 'localhost', port, listeningListener = listener.bind(null, host, port) } = options
+  const { host = 'localhost', port } = options
   const app = new Koa<State, Context>()
 
   app.use(logger)
 
-  app.use((ctx) => {
-    ctx.logger.info('1324')
-  })
+  return http.createServer(app.callback()).listen(port, host, () => {
+    const { listeningListener } = options
+    if (typeof listeningListener === 'function') listeningListener()
 
-  return http.createServer(app.callback()).listen(port, host, listeningListener)
+    if (process.env.__DEV__) console.log(`Server is running at: http://${host}:${port}`)
+  })
 }
--- src/common/middlewares/logger/index.ts
+++ src/common/middlewares/logger/index.ts
@@ -1,8 +1,17 @@
 import { Middleware } from 'koa'
 import pino from 'pino'
+import type { State, Context } from '../../typings'
 
-export const logger: Middleware = (ctx, next) => {
-  ctx.logger = pino()
+export const logger: Middleware<State, Context> = (ctx, next) => {
+  ctx.logger = pino({
+    level: process.env.LOG_LEVEL || 'info',
+    prettyPrint: {
+      translateTime: true,
+    },
+  })
+
+  ctx.logger.trace(`${ctx.request.method} ${ctx.path}`)
+
   return next().catch((e) => {
     ctx.logger.error(e)
   })
